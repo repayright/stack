c6eb568e01d670b4a2d3f70bfd82745b1db841e0
David Ragot <david.ragot14@gmail.com>
2023-07-19 11:50:56 +0200

    fix: remove all idiomatic naming

--- components/fctl/pkg/controller.go
+++ components/fctl/pkg/controller.go
@@ -1,15 +1,24 @@
 package fctl
 
 import (
-	"github.com/spf13/cobra"
+	"context"
+	"flag"
 )
 
 type Renderable interface {
-	Render(cmd *cobra.Command, args []string) error
+	Render() error
 }
 type Controller[T any] interface {
 	GetStore() T
-	Run(cmd *cobra.Command, args []string) (Renderable, error)
+	GetFlags() *flag.FlagSet
+	SetArgs([]string)
+
+	// Not present from creation of the controller
+	// The context is set by the command
+	GetContext() context.Context
+	SetContext(context.Context)
+
+	Run() (Renderable, error)
 }
 type ExportedData struct {
 	Data interface{} `json:"data"`
