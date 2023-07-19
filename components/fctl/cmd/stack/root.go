a6cdbac934860fdb8e85aebaf69884cd1994a8e8
David Ragot <david.ragot14@gmail.com>
2023-07-19 14:24:56 +0200

    fix: global flags appears as global flags in cobra output, & same for scopes `flags`

--- components/fctl/cmd/stack/root.go
+++ components/fctl/cmd/stack/root.go
@@ -16,7 +16,6 @@ func NewCommand() *cobra.Command {
 			NewShowCommand(),
 			NewRestoreStackCommand(),
 		),
-		fctl.WithGoPersistentFlagSet(fctl.GlobalFlags),
 		fctl.WithScopesFlags(&fctl.Organization),
 	)
 }
